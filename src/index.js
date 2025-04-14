e0ccb0a64529a3edb6a269a8587e7980ebae45f1
SibongileT88 <sibongilet@beskbridge.com>
2025-04-14 12:29:31 +0200

    Added the typewriter plugin and basic javascript to the form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: [
+      "We become what we think about,energy flows where attention goes",
+    ],
+    autoStart: true,
+    delay: 5,
+    cursor: null,
+  });
+}
+let quoteElement = document.querySelector("#quote-generator");
+quoteElement.addEventListener("submit", generateQuote);
